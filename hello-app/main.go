153b8138f497d4c7848a3755d67acb590560fae9
phanithi <pnraju.20120@gmail.com>
2018-04-19 21:13:46 +0530

    Message updated

--- hello-app/main.go
+++ hello-app/main.go
@@ -46,6 +46,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
 	fmt.Fprintf(w, "Hello, Nagaraju!\n")
+	fmt.Fprintf(w, "Welcome to Kubernates World!!\n")
 	fmt.Fprintf(w, "Version: 1.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 }
